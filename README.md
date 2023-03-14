005333262f4b7baf435a4e53b77435e9d2f64643
Alex Nguetcha <nguetchaalex@gmail.com>
2023-03-14 15:49:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Intouch Payment Library
+
+The Intouch Payment Library is a PHP library that provides a simple interface for making payments with mobile money service providers that are supported by the Intouch platform. This library handles the complexities of interacting with the Intouch API, allowing you to focus on building your application.
+
+## Installation
+
+To install the Intouch Payment Library, you can use [Composer](https://getcomposer.org/), a package manager for PHP. Run the following command in your project directory:
+
+```
+composer require alexnguetcha/intouch
+```
+
+## Complete example
+
+Here is an complete example of a use case
+
+```php
+$intouch = Intouch::credentials(
+    username: 'username',
+    password: 'password',
+    loginAgent: 'loginAgent',
+    passwordAgent: 'passwordAgent',
+    intouchId: 'passwordAgent'
+)->callback('https://app.test/confirm-payment')
+    ->amount(100)
+    ->phone(6xxxxxx)
+    ->operator('ORANGE')
+    ->makeMerchantPayment(
+        [
+            "recipientEmail" => "john@example.com",
+            "recipientFirstName" => "John",
+            "recipientLastName" => "Doe",
+        ]
+    );
+
+
+if ($intouch->isInitiated()) {
+    // Your transaction has been initiated by intouch API
+    // echo (string)($intouch->getResult()->getBody());
+} else {
+    // something went wrong
+    // echo $intouch->getError()['request'];
+    // echo $intouch->getError()['response'];
+}
+```
+
+
+## Usage
+
+To use the Intouch Payment Library, you must first obtain credentials from the [Intouch platform](https://intouchgroup.net/). These credentials include a username, password, login agent, password agent, and Intouch ID. You can then create an instance of the `Intouch` class, passing in your credentials:
+
+```php
+use AlexNguetcha\Intouch\Intouch;
+
+$intouch = Intouch::credentials(
+    username: 'your-username',
+    password: 'your-password',
+    loginAgent: 'your-login-agent',
+    passwordAgent: 'your-password-agent',
+    intouchId: 'your-intouch-id'
+);
+```
+
+## Initiating a Payment
+
+Once you have created an instance of the `Intouch` class, you can initiate a payment using the `makeMerchantPayment` method. This method takes an array of payment information, including the amount to be paid, the mobile money account to be debited, and the recipient information:
+
+Before initiating a payment, you need to set the payment details using the following methods:
+
+```php
+$intouch->amount(100); // Set the amount to be paid
+$intouch->phone('1234567890'); // Set the mobile money account to be debited
+$intouch->operator('MTN'); // Set the mobile money operator for the transaction
+$intouch->callback('https://example.com/payment/callback');
+```
+
+
+### Callbacks
+
+Intouch sends a notification to your application when a payment is completed. You can set a callback URL using the `callback` method. This URL will be called by Intouch with a payload containing information about the completed payment:
+
+```php 
+$intouch->makeMerchantPayment([
+    "recipientEmail" => "john@example.com",
+    "recipientFirstName" => "John",
+    "recipientLastName" => "Doe",
+    "phone" => "1234567890",
+    "amount" => 100,
+    "operator" => "MTN",
+    "reason" => "Payment for goods and services",
+]);
+
+```
+
+
+
+```php 
+$intouch->callback('https://your-app.com/intouch-callback');
+```
+
+## Handling the Response
+
+You can check if a payment was successfully initiated using the isInitiated method. If the payment was initiated, you can access the response using the getResult method. If something went wrong, you can access error information using the getError method:
+
+
+```php
+if ($intouch->isInitiated()) {
+    // Your transaction has been initiated by intouch API
+    echo (string)($intouch->getResult()->getBody());
+} else {
+    // something went wrong
+    // echo $intouch->getError()['request'];
+    echo $intouch->getError()['response'];
+}
+```
+
+## Conclusion
+
+The Intouch Payment Library provides a simple and convenient way to interact with the Intouch API and make mobile money payments. By following the steps outlined in this documentation, you can easily integrate mobile money payments into your PHP application.
---
+++ src/Intouch.php
@@ -0,0 +1,434 @@
+<?php
+
+namespace AlexNguetcha\Intouch;
+
+use Error;
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Psr7;
+
+/**
+ * Class Intouch
+ * A PHP wrapper for Intouch API to perform cash-in, cash-out and balance transactions.
+ * 
+ * @package AlexNguetcha\Intouch
+ */
+class Intouch
+{
+    /**
+     * Intouch Cameroon Service Code for cash-in transaction
+     */
+    const OM_CASH_IN = "CASHINOMCMB2BDIST";
+    const MOMO_CASH_IN = "CASHINMTNCM2_DYNATECH";
+
+    /**
+     * Intouch Cameroon Service Code for cash-out transaction
+     */
+    const OM_CASH_OUT = "CASHOUTOMCMB2BDIST";
+    const MOMO_CASH_OUT = "CASHOUTMTNCM2_DYNATECH";
+
+    /**
+     * URL for merchant payment endpoint
+     */
+    private const GUTOUCH_API_URL = "https://api.gutouch.com/dist/api/touchpayapi/v1/[INTOUCH_ID]/transaction?loginAgent=[LOGIN_AGENT]&passwordAgent=[PASSWORD_AGENT]";
+
+    /**
+     * URL for cash-in payment endpoint
+     */
+    private const GUTOUCH_API_CASHIN_URL = "https://api.gutouch.com/v1/[INTOUCH_ID]/cashin";
+
+    /**
+     * URL for checking Intouch balance endpoint
+     */
+    private const GUTOUCH_API_GETBALANCE = "https://api.gutouch.com/v1/[INTOUCH_ID]/get_balance";
+
+    /**
+     * Username and Password provided by Intouch API
+     *
+     * @var string $username
+     * @var string $password
+     */
+    private $username;
+    private $password;
+
+    /**
+     * Login Agent and Password Agent provided by Intouch API
+     * 
+     * @var string $loginAgent
+     * @var string $passwordAgent
+     */
+    private $loginAgent;
+    private $passwordAgent;
+
+    /**
+     * Partner ID
+     * 
+     * @var string|null $partnerId
+     */
+    private $partnerId = null;
+
+    /**
+     * Intouch ID
+     * 
+     * @var string $intouchId
+     */
+    private $intouchId;
+
+    /**
+     * Callback URL to call when payment is successful
+     * 
+     * @var string $callbackUrl
+     */
+    private $callbackUrl;
+
+    /**
+     * Amount of the payment
+     * 
+     * @var string $amount
+     */
+    private $amount;
+
+    /**
+     * Phone number of the recipient
+     * 
+     * @var string $phone
+     */
+    private $phone;
+
+    /**
+     * API endpoint
+     * 
+     * @var string $endpoint
+     */
+    private $endpoint;
+
+    /**
+     * Intouch's service code for payment
+     * 
+     * @var string $serviceCode
+     */
+    private $serviceCode;
+
+    /**
+     * ISP operator (e.g. ORANGE, MTN)
+     * 
+     * @var string $operator
+     */
+    private $operator;
+
+    /**
+     * ID provided by the client
+     * 
+     * @var string|null $idFromClient
+     */
+    private $idFromClient = null;
+
+    /**
+     * Supported ISP operators
+     * 
+     * @var array $SUPPORTED_OPERATORS
+     */
+    private const SUPPORTED_OPERATORS = ['ORANGE', 'MTN'];
+
+    private $apiResult = null;
+    private $apiError = null;
+
+    private $initiated = false;
+
+    /**
+     * @param string $username
+     * @param string $password
+     * 
+     */
+    public function __construct($username, $password, $loginAgent, $passwordAgent, $intouchId)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->loginAgent = $loginAgent;
+        $this->passwordAgent = $passwordAgent;
+        $this->intouchId = $intouchId;
+    }
+
+    public function getError()
+    {
+        return $this->apiError;
+    }
+
+    private function apiResult(mixed $result)
+    {
+        $this->apiResult = $result;
+    }
+
+    private function setError(mixed $error)
+    {
+        $this->apiError = $error;
+    }
+
+    public function getResult()
+    {
+        return $this->apiResult;
+    }
+
+    private function endpoint(string $endpoint)
+    {
+        $this->endpoint = $endpoint;
+        return $this;
+    }
+
+    private function serviceCode(string $serviceCode)
+    {
+        $this->serviceCode = $serviceCode;
+        return $this;
+    }
+
+    public function amount(string $amount)
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function phone(string $phone)
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function callback(string $url)
+    {
+        $this->callbackUrl = $url;
+        return $this;
+    }
+
+    public function partnerId(string $partnerId)
+    {
+        $this->partnerId = $partnerId;
+        return $this;
+    }
+
+    public function operator(string $operator)
+    {
+        $operator = strtoupper($operator ?? '');
+        if (!in_array($operator, self::SUPPORTED_OPERATORS)) {
+            throw new Exception("Unsupported operator: $operator, supported operators are " . implode(",", self::SUPPORTED_OPERATORS));
+        }
+        $this->operator = $operator;
+        return $this;
+    }
+
+    public static function credentials(string $username, string $password, string $loginAgent, string $passwordAgent, $intouchId): Intouch
+    {
+        return new Intouch($username, $password, $loginAgent, $passwordAgent, $intouchId);
+    }
+
+    private function replaceUrlAgentCredentials(string $url): string
+    {
+        $url =  str_replace('[LOGIN_AGENT]', $this->loginAgent, $url);
+        $url =  str_replace('[PASSWORD_AGENT]', $this->passwordAgent, $url);
+        $url =  str_replace('[INTOUCH_ID]', $this->intouchId, $url);
+        return $url;
+    }
+
+    private function setTheRighServiceCodeAndEndpoint($for = 'merchant' | 'cashin' | 'cashout' | 'balance')
+    {
+        // check the operator
+        if ($for !== 'balance' && $this->operator === null) {
+            throw new Exception('You must provide an operator for ' . $for . ' payment.');
+        }
+
+        // set the right service code
+        switch ($for) {
+            case 'merchant':
+                if ($this->operator == self::SUPPORTED_OPERATORS[0]) {
+                    // ORANGE
+                    $this->serviceCode('CM_PAIEMENTMARCHAND_OM_TP');
+                } else if ($this->operator == self::SUPPORTED_OPERATORS[1]) {
+                    // MTN
+                    $this->serviceCode('PAIEMENTMARCHAND_MTN_CM');
+                }
+                $this->endpoint($this->replaceUrlAgentCredentials(self::GUTOUCH_API_URL));
+                break;
+            case 'cashin':
+                if ($this->operator == self::SUPPORTED_OPERATORS[0]) {
+                    // ORANGE
+                    $this->serviceCode('CASHINOMCMB2BDIST');
+                } else if ($this->operator == self::SUPPORTED_OPERATORS[1]) {
+                    // MTN
+                    $this->serviceCode('CASHINMTNCM2_DYNATECH');
+                }
+                $this->endpoint($this->replaceUrlAgentCredentials(self::GUTOUCH_API_CASHIN_URL));
+                break;
+            case 'balance':
+                $this->endpoint($this->replaceUrlAgentCredentials(self::GUTOUCH_API_GETBALANCE));
+                break;
+            default:
+                # code...
+                break;
+        }
+    }
+
+    private function isValidPhoneNumber($phoneNumber): bool
+    {
+        return preg_match('/^6\d{8}$/', $phoneNumber) !== false;
+    }
+
+    private function isValidUrl($url): bool
+    {
+        return preg_match('/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/', $url) !== false;
+    }
+
+    private function checkMiminumRequirements($for)
+    {
+        if (!is_numeric($this->amount) && in_array($for, ['merchant', 'cashin', 'cashout', 'balance'])) {
+            throw new Exception('You must provide a valid amount for the transaction.');
+        } else if (intval($this->amount) < 100) {
+            throw new Exception('Transaction amount must be greather than 100 XAF');
+        }
+
+        if (is_null($this->partnerId) && in_array($for, ['cashin', 'balance'])) {
+            throw new Exception('You must provide a valid Intouch partner id.');
+        }
+
+        if (!$this->isValidPhoneNumber($this->phone) && in_array($for, ['merchant', 'cashin', 'cashout', 'balance'])) {
+            throw new Exception('You must provide a valid phone number 6abcdefgh.');
+        }
+
+        if (!$this->isValidUrl($this->callbackUrl) && in_array($for, ['merchant', 'cashin', 'cashout', 'balance'])) {
+            throw new Exception('You must provide a valid callback url.');
+        }
+    }
+
+    /**
+     * Initiate a merchant payment
+     * @param array additionnalInfos
+     */
+    public function makeMerchantPayment(array $additionnalInfos = []): Intouch
+    {
+
+        $this->checkMiminumRequirements('merchant');
+        $this->setTheRighServiceCodeAndEndpoint('merchant');
+
+        $payload = [
+            'idFromClient' => $this->idFromClient ?? time(),
+            'amount' => $this->amount,
+            'callback' => $this->callbackUrl,
+            'recipientNumber' => $this->phone,
+            'serviceCode' => $this->serviceCode,
+            'additionnalInfos' => $additionnalInfos
+        ];
+
+        $client = new Client([]);
+        try {
+            $result = $client->request('PUT', $this->endpoint, [
+                'json' => $payload,
+                'auth' => [$this->username, $this->password, 'digest'],
+                'verify' => false
+            ]);
+            $this->apiResult($result);
+        } catch (ConnectException $e) {
+            $this->setError([
+                'request' => Psr7\Message::bodySummary($e->getRequest(), 10000),
+                'response' => 'connection cannot be established'
+            ]);
+        } catch (Exception $e) {
+            $this->setError([
+                'request' => Psr7\Message::bodySummary($e->getRequest(), 10000),
+                'response' => Psr7\Message::bodySummary($e->getResponse(), 10000)
+            ]);
+        }
+        return $this;
+    }
+
+    public function sendMoney()
+    {
+
+        $this->checkMiminumRequirements('cashin');
+        $this->setTheRighServiceCodeAndEndpoint('cashin');
+
+        $payload = [
+            "service_id" => $this->serviceCode,
+            "recipient_phone_number" => $this->phone,
+            "amount" => $this->amount,
+            "partner_id" => $this->partnerId,
+            "partner_transaction_id" => $this->idFromClient ?? time(),
+            "login_api" => $this->loginAgent,
+            "password_api" => $this->passwordAgent,
+            "call_back_url" => $this->callbackUrl
+        ];
+
+        $client = new Client([]);
+
+        try {
+            $result = $client->request('POST', $this->endpoint, [
+                'json' => $payload,
+                'auth' => [$this->username, $this->password, 'basic'],
+                'verify' => false
+            ]);
+            $this->apiResult($result);
+        } catch (ConnectException $e) {
+            $this->setError([
+                'request' => Psr7\Message::bodySummary($e->getRequest(), 10000),
+                'response' => 'connection cannot be established'
+            ]);
+        } catch (Exception $e) {
+            $this->setError([
+                'request' => Psr7\Message::bodySummary($e->getRequest(), 10000),
+                'response' => Psr7\Message::bodySummary($e->getResponse(), 10000)
+            ]);
+        }
+        return $this;
+    }
+
+    public function getBalance()
+    {
+        $this->checkMiminumRequirements('balance');
+        $this->setTheRighServiceCodeAndEndpoint('balance');
+
+        $payload = [
+            "partner_id" => $this->partnerId,
+            "login_api" => $this->loginAgent,
+            "password_api" => $this->passwordAgent,
+        ];
+
+        $client = new Client([]);
+
+        $result = null;
+        try {
+            $result = $client->request('POST', $this->endpoint, [
+                'json' => $payload,
+                'auth' => [$this->username, $this->password, 'basic'],
+                'verify' => false
+            ]);
+            // var_dump($result);
+            $this->apiResult($result);
+        } catch (ConnectException $e) {
+            $this->setError([
+                'request' => Psr7\Message::bodySummary($e->getRequest(), 10000),
+                'response' => 'connection cannot be established'
+            ]);
+        } catch (Exception $e) {
+            $this->setError([
+                'request' => Psr7\Message::bodySummary($e->getRequest(), 10000),
+                'response' => Psr7\Message::bodySummary($e->getResponse(), 10000)
+            ]);
+        }
+        return $this;
+    }
+
+    public function isInitiated(): bool
+    {
+        if ($this->apiResult === null) return false;
+
+        $body = (string)($this->apiResult->getBody());
+        $result = get_object_vars(json_decode($body));
+        $initiated =  is_array($result) && ((array_key_exists('status', $result) && $result['status'] === 'INITIATED')
+            || array_key_exists('amount', $result));
+        if (!$initiated) {
+            $this->setError([
+                'request' => array_key_exists('request', $this->getError()) ? $this->getError()['request'] : null,
+                'response' => $result
+            ]);
+        }
+        return $initiated;
+    }
+}
